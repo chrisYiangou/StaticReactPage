1d2aa0e4c24727a2ee8a1a9ce0b664358b88d9f0
chrisYiangou <chrisyiangou100@gmail.com>
2022-06-07 15:29:06 +0100

    Added most Css for Nav, working on Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,17 @@ import React from "react"
 
 export default function Main() {
     return (
-        <h1>
-        Main component
-        </h1>
+        <div className="main-body">
+            <h1>
+            Fun Facts about React
+            </h1>
+            <ul className="fact-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands if enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,17 @@
 import React from "react"
+import Logo from "../pictures/ReactIcon.png"
 
 export default function Navbar() {
     return (
-        <div className="navbar">
-            <h1>
-            Navbar component
-            </h1>
-        </div>
-    )
+        <nav className="navbar">
+            <img className="react-pic" src={Logo} alt ="React Logo"  />
+            <h3 className="nav-title">
+                ReactFacts
+            </h3>
+            <h4 className="nav-text">
+                React Course - Project 1
+            </h4>
+        </nav>
+  
+    );
 }
